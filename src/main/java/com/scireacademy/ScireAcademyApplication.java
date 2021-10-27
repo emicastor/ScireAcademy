0abe2a2f15cbbe73d23dbe8b92dcb708aabfa985
emiliano castor <castoremi12@gmail.com>
2021-10-27 10:52:41 -0300

    creacion del proyecto

---
+++ src/main/java/com/scireacademy/ScireAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.scireacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScireAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScireAcademyApplication.class, args);
+	}
+
+}
